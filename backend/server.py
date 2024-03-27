98de82a677bbd3ccaa079856c2b55e6f96695fc4
Umar Khan <muk779912@gmail.com>
2024-03-27 15:42:46 -0600

    done exercise A

--- backend/server.py
+++ backend/server.py
@@ -7,9 +7,13 @@ app = Flask(__name__)
 def load_products():
     with open('products.json', 'r') as f:
         return json.load(f)['products']
-    
+
+def save_products(products):
+    with open('products.json', 'w') as f:
+        json.dump({"products": products}, f)
+
 @app.route('/products', methods=['GET'])
-@app.route('/products/<int:products_id>', methods=['GET'])
+@app.route('/products/<int:products_id>', methods=['GET'])  
 def get_products(products_id=None):
     products = load_products()
     if products_id is None:
@@ -17,17 +21,36 @@ def get_products(products_id=None):
     else:
         product = next((p for p in products if p['id'] == products_id), None)
         return jsonify(product) if product else ('', 404)
-    
 
 @app.route('/products/add', methods=['POST'])
 def add_product():
-    new_products = request.json
+    new_product = request.json
     products = load_products()
-    new_products['id'] = len(products) + 1
-    products.append(new_products)
-    with open('products.json', 'w') as f:
-        json.dump({"products": products}, f)
-    return jsonify(new_products), 201
+    new_product['id'] = len(products) + 1
+    products.append(new_product)
+    save_products(products)
+    return jsonify(new_product), 201
+
+@app.route('/products/update/<int:products_id>', methods=['PUT'])
+def update_product(products_id):
+    updated_product = request.json
+    products = load_products()
+    for product in products:
+        if product['id'] == products_id:
+            product.update(updated_product)
+            save_products(products)
+            return jsonify(product), 200
+    return jsonify({"error": "Product not found"}), 404
+
+@app.route('/products/remove/<int:products_id>', methods=['DELETE'])
+def remove_product(products_id):
+    products = load_products()
+    for product in products:
+        if product['id'] == products_id:
+            products.remove(product)
+            save_products(products)
+            return jsonify({"message": "Product removed successfully"}), 200
+    return jsonify({"error": "Product not found"}), 404
 
 @app.route('/product-images/<path:filename>')
 def get_image(filename):
